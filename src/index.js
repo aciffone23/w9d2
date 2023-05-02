8df71f1378daf7c793a6a7ec4c784a9a1a987943
Matt Pierson <matt.pierson789@gmail.com>
2023-05-02 15:41:03 -0400

    message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-Add your import statements for View and Game here
+
+//portal to render the js.  Grabs the document by the 
+// id from index.html
+
+import Game from "../ttt_node/game.js";
+import View from "./ttt-view.js";
+
+// We can put in invocation of a game in this file
+
+
+// Load up index.html and then render the js
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+  const g = new Game();   
+  const el1 = document.querySelector('.ttt')
+  const v1 = new View(g, el1)
+
+  //.queryselector
+
+
 });
+
+
+// select the figure or canvas class - it can be any HTML node
+// and ensure that we are passing that into the view .  This is like the yield
+//mapped onto the figure node via the event listener.
+
+// DOM content loaded waits for the HTML to load and then maps the JS on
--- src/ttt-view.js
+++ src/ttt-view.js
@@ -1,11 +1,33 @@
+// import { makeGrid } from "../ttt_node/board";
+
 class View {
   constructor(game, el) {
+
+    this.game = game
+    this.el = el
+    this.setupBoard();
+  
   }
   
   setupBoard() {
+
+    const ul =  document.createElement("ul")
+    ul.classList.add('grid');
+    this.el.appendChild(ul);
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+
+        const li = document.createElement('li');
+        ul.appendChild(li);
+
+    }
+  }
   }
   
   handleClick(e) {
+
+    
   }
 
   makeMove(square) {
